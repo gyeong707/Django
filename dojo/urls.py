d47d045a4eea9b9c435dab4e23b2d148c30ce0ff
gyeong707 <kang9260@naver.com>
2019-01-22 22:45:06 +0900

    FBV 함수 기반 뷰의 네 가지 패턴

--- dojo/urls.py
+++ dojo/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     #이거 그냥 path로는 어떻게 쓰지? Converter 이용하는건가??
 
     re_path(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)$', views.hello),
-    
-]
+    path('list1/', views.post_list1),
+    path('list2/', views.post_list2),
+    path('list3/', views.post_list3),
+    path('excel/', views.excel_download),
+]   
--- dojo/views.py
+++ dojo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
+import os
 
 # Create your views here.
 def mysum(request, numbers):
@@ -11,4 +12,36 @@ def mysum(request, numbers):
 def hello(request, name, age):
     return HttpResponse('안녕하세요. {}. {}살이시네요' .format (name, age))
 
-    
+#http 리스폰스#
+def post_list1(request):
+    name = "강미경"
+    return HttpResponse('''
+    <h1> AskDjango </h1>
+    <p> {name} </p>
+    <p> 여러분의 파이썬&장고 페이스메이커가 되어드리겠습니다.</p>
+    '''.format(name=name))
+
+
+#템플릿 응답#
+def post_list2(request):
+    name = "미경"
+    return render(request , 'dojo/post_list.html', {'name':name})
+
+
+#제이슨 응답#
+def post_list3(request):
+    return JsonResponse({
+        'message' : '안녕 파이썬&장고', 'items' : ['파이썬', '장고', 'Celery', 'Azure'],
+    }, json_dumps_params={'ensure_ascii': False})
+
+
+#파일 다운로드 응답#
+def excel_download(request):
+    filepath = 'C:/django/askdjango/test.xlsx'
+    filename = os.path.basename(filepath)
+    #파일 이름부분만 추출
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel')
+        #기본이 text/html임. 근데 우리는 excel 형식이니까 application~으로 지정해주어야 하는 것.
+        response['Content-Disposition'] = 'attachment; filename="{}"' .format(filename)
+        return response
