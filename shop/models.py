b9fcecc0d6361207733519ea8ae8a312c0088ff7
gyeong707 <kang9260@naver.com>
2019-01-29 01:23:04 +0900

    shop.models.py에related_name='+' 옵션추가

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete='CASCADE', related_name='+')
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
