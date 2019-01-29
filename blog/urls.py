caa44426615aabf9bbf4ab66fc987fa2fcc504dd
gyeong707 <kang9260@naver.com>
2019-01-30 00:23:36 +0900

    blog에 url reverse 수행

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import re_path, path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list),
-    path('<int:id>/', views.post_detail),
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
 ]
 #url이 아닌 path 사용
