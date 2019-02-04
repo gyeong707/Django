dd5beb2562a18923c1441bb17c0b52550ea6806e
gyeong707 <kang9260@naver.com>
2019-02-04 17:54:40 +0900

    CBV : DeleteView 구현

--- blog/urls.py
+++ blog/urls.py
@@ -8,8 +8,9 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views_cbv.post_list, name='post_list'),
-    # path('<int:id>/', views.post_detail, name='post_detail'),
-    path('<pk>/', views_cbv.post_detail),\
+
+    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('cbv/<pk>/', views_cbv.post_detail),
 
     path('<int:id>/edit/', views.post_edit, name='post_edit'),
     path('cbv/<pk>/edit/', views_cbv.post_edit, name='post_edit'),
@@ -18,4 +19,5 @@ urlpatterns = [
     path('new/', views.post_new, name='post_new'),
     path('cbv/new/', views_cbv.post_new),
 
+    path('cbv/<pk>/delete/', views_cbv.post_delete),
 ]
--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,6 +1,6 @@
 from .models import Post
 from django import forms
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import DeleteView, ListView, CreateView, DetailView, UpdateView
 
 
 class PostForm(forms.ModelForm):
@@ -14,7 +14,8 @@ class PostCreateView(CreateView):
     form_class = PostForm
 
 
-post_new = PostCreateView.as_view()
+post_new = CreateView.as_view(model=Post, fields='__all__')
 post_list = ListView.as_view(model=Post, paginate_by=10)
 post_detail = DetailView.as_view(model=Post)
 post_edit = UpdateView.as_view(model=Post, fields='__all__')
+post_delete = DeleteView.as_view(model=Post, success_url="/")
