7b7fbeeeecd7afb1d5014d36c1902437a65a4382
gyeong707 <kang9260@naver.com>
2019-02-04 21:55:50 +0900

    퀴즈를 맞춰야 로그인되도록 암호걸기

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,12 +2,15 @@ from django.urls import path, re_path, include
 from . import views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
+from .forms import LoginForm
+
 
 urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(
-        template_name='accounts/login_form.html'), 
+        template_name='accounts/login_form.html',
+        authentication_form = LoginForm), 
         name='login'),
     path('logout/', auth_views.LogoutView.as_view(
         next_page=settings.LOGIN_URL), name='logout'),
