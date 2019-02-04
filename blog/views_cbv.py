b16ed5ad0c392eb97a8fb2c412b1995c68ecabe2
gyeong707 <kang9260@naver.com>
2019-02-04 18:02:42 +0900

    reverse_lazy 구현

--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,7 +1,7 @@
 from .models import Post
 from django import forms
 from django.views.generic import DeleteView, ListView, CreateView, DetailView, UpdateView
-
+from django.urls import reverse_lazy
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -18,4 +18,4 @@ post_new = CreateView.as_view(model=Post, fields='__all__')
 post_list = ListView.as_view(model=Post, paginate_by=10)
 post_detail = DetailView.as_view(model=Post)
 post_edit = UpdateView.as_view(model=Post, fields='__all__')
-post_delete = DeleteView.as_view(model=Post, success_url="/")
+post_delete = DeleteView.as_view(model=Post, success_url=reverse_lazy('blog:post_list'))
